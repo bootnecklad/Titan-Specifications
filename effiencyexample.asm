9aad75a77a5f5bce15b9b6e096eda288a2a853b0
bootnecklad <bootnecklad@gmail.com>
2012-01-11 21:51:23 +0000

    More proving that the new ISA is better at trivial tasks as well

---
+++ effiencyexample.asm
@@ -0,0 +1,78 @@
+; One of first Titan Assembly programs ever written
+; I found it in an old backup of website from when I
+; Began the project
+; First is the ORIGINAL program and the assembled machine code
+; Then the NEW assembly program followed by the assembled machine code
+; I believe there has been much improvement
+;
+; Number of bytes in old program: 19 bytes
+; Number of bytes in new program: 9 bytes
+; (9/(9+19)*100 = 32% more efficient!
+;
+; This shows that the new ISA is a success in this case. :>
+;
+; Marc Cleave - 11/01/2012
+
+
+ADDR   ASM             COMMENTS
+00   jmp 0x0005    /Skips two numbers to be added together
+03   0x56          /Number one
+04   0x7f          /Number two
+05   ldm 0x0003    /
+06   psh a         /
+07   pop b         /Loads number one into B
+08   ldm 0x0004    /Loads number two into accumulator
+0b   add           /Adds two numbers
+0c   stm 0xffff    /Writes accumulator to I/O port 256, LED display on front panel
+0f   jmp 0x000f    /Continuous loop
+
+
+Machine Code
+      
+1001 0000
+0000 0000
+0000 0101
+0101 0110
+0111 1111
+1110 0010
+0000 0000
+0000 0011
+1110 0001
+0000 0000
+0000 0100
+0001 0000
+1111 0001
+1111 1111
+1111 1111
+0000 0000
+1001 0000
+0000 0000
+0000 1000
+
+
+
+
+
+
+BEGIN:
+   LDC R0,0x56   ; Loads first constant to be added
+   LDC R1,0x7F   ; Loads second constant to be added
+   ADD R0,R1     ; Adds the two values in R0 and R1
+   STM R0,0xFFFF ; Outputs new value to LED I/O Port   
+
+END:
+   JMP END       ; Continuous loop
+
+
+
+Machine code:
+
+1101 0000
+0101 0110
+1101 0001
+0111 1111
+0000 0000
+0000 0001
+1111 0000
+1111 1111
+1111 1111
