3276a85efea958a2770cc506ce6409964833504c
bootnecklad <bootnecklad@gmail.com>
2012-01-27 18:38:48 +0000

    Changed string terminator from 0x03 to 0x00

--- hellouniverse.asm
+++ hellouniverse.asm
@@ -3,17 +3,15 @@
 ; (C) Marc Cleave 27/01/2012
 
 START:
-   LDC R2,0x03  ; ASCII data for END OF TEXT
-   LDC R3,0x01  ; Needed for incrementing string byte count
-   CLR R4       ; Clears R3 for indexed loading of bytes!!111
+   LDC R2,0x01  ; Needed for incrementing string byte count
+   CLR R3       ; Clears R3 for indexed loading of bytes!!111
 
 
 LOOP:
-   MOV R2,R1             ; Testing for END OF TEXT is destructive, so must be restored
-   LDI R0,STRING[R4]     ; Gets next byte from string
-   XOR R0,R1             ; Tests if next value in string is 0x03
-   JPZ END               ; If 0x03 then end of the string!
-   ADD R3,R4             ; Next address to get string must be +1 from previous
+   LDI R0,STRING[R3]     ; Gets next byte from string
+   TST R0                ; Tests byte fetched from string
+   JPZ END               ; If 0x00 then end of the string!
+   ADD R2,R3             ; Next address to get string must be +1 from previous
    STM R0,SERIAL_PORT_0  ; Outputs ASCII data to 
    JMP LOOP              ; Time to get next character!
 
