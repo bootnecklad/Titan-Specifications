86dc17bab6a9c65d7f4f0719d4d1293725210e00
bootnecklad <bootnecklad@gmail.com>
2012-02-12 14:51:21 +0000

    Fixed READ routine and some stuff

--- monitor.asm
+++ monitor.asm
@@ -75,70 +75,80 @@ READ:
    LDI R0              ; Loads the byte to be read, uses indexed load with NO offset, address in R1 and R2.
    PSH R0              ; Saves byte before manipulation
    LDC R1,0x0F         ; Part of byte to remove
-   AND R1,R0           ; Upper nybble removed, ie bits UNSET, lower nybble left intact
-   LDC R1,0x09
-   SUB R0,R1           ; Does R1 = R1 - R0, if R0 is less than 9 then sign and zero not set.
-   JPZ LOW_NYBBLE_30   ; Lower nybble is 9 so 0x30 must be added to create an ASCII byte
-   JPS LOW_NYBBLE_37   ; R0 was greater than 9 so 0x37 must be added to create an ASCII byte
-   JMP LOW_NYBBLE_30   ; R0 was less than 9 so nybble was less than 9, so 0x30 must be added to create an ASCII byte
-   LOW_NYBBLE_30:
-   LDC R1,0x30
-   ADD R1,R0          ; Adds 0x30 to nybble to create ASCII data
-   MOV R0,RE          ; Saves byte to be outputted
-   JMP HIGH_NYBBLE
-   LOW_NYBBLE_37:
-   LDC R1,0x37
-   ADD R1,R0           ; Adds 0x37 to nybble to create ASCII data
-   MOV R0,RF           ; Saves byte to be outputted
-   JMP HIGH_NYBBLE
-   LDC R1,0xF0         ; Nybble of byte to be removed
-   POP R0              ; Returns unaltered value
-   AND R1,R0           ; Lower nybble removed
-   SHR R0
-   SHR R0
-   SHR R0
-   SHR R0              ; Shifted right four times to move it to lower nybble
-   LDC R1,0x09
-   SUB R0,R1           ; Does R1 = R1 - R0, if R0 is less than 9 then sign and zero not set.
-   JPZ HIGH_NYBBLE_30  ; Lower nybble is 9 so 0x30 must be added to create an ASCII byte
-   JPS HIGH_NYBBLE_37  ; R0 was greater than 9 so 0x37 must be added to create an ASCII byte
-   JMP HIGH_NYBBLE_30  ; R0 was less than 9 so nybble was less than 9, so 0x30 must be added to create an ASCII byte
-   HIGH_NYBBLE_30:
-   LDC R1,0x30
-   ADD R1,R0
-   MOV R0,RE           ; Saves byte to be outputted
-   JMP OUTPUT          ; Wooo!
-   HIGH_NYBBLE_37:
-   LDC R1,0x37
-   ADD R1,R0
-   MOV R0,RE           ; Saves byte etc
-   OUTPUT:
-   STM RE,SERIAL_PORT_0  ; Ouputs high nybble
-   STM RF,SERIAL_PORT_0  ; Outputs low nybble
+   AND R0,R1           ; Upper nybble removed, ie bits UNSET, lower nybble left intact
+   LDI R2,HASH_TABLE_BYTE[R1]  ; Data byte used to fetch the ASCII equvilent, ie if data is 0x5 then 0x35 is needed to be output to terminal
+   POP R0
+   LDC R1,0xF0
+   AND R0,R1           ; Lower nybble removed, bits set to 0
+   SHR R1
+   SHR R1
+   SHR R1
+   SHR R1              ; Shift the byte right four times, moves data to lower nybble
+   LDI R3,HASH_TABLE_BYTE[R1]  ; Data -> ASCII complete
+   STI R3,SERIAL_PORT_0   ; Output high ASCII byte to serial terminal
+   STI R2,SERIAL_PORT_0   ; Outputs low ASCII byte
    JMP BEGIN
 
+
 CREATE_ADDRESS:
-   POP RE
-   POP RF     ; DONT BREAK THE RETURN ADDRESS!
-   POP R0       ; Low nybble of low address
-   LDC R1,0x30  ; Remove constant from ASCII value, makes table smaller.
-   SUB R1,R0    ; R0 = R0 - R1
-   LDI R2,HASH_TABLE[R0]
+   POP RA
+   POP RB     ; DONT BREAK THE RETURN ADDRESS!
+   POP R1       ; Low nybble of low address
+   LDC R0,0x30  ; Remove constant from ASCII value, makes table smaller.
+   SUB R0,R1    ; R1 = R1 - R0
+   LDI R2,HASH_TABLE[R1]
    POP R0       ; High nybble of low address
    SUB R1,R0    ; Removes constant
-   LDI R3,HASH_TABLE[R0]
+   LDI R3,HASH_TABLE[R1]
    ADD R3,R2    ; Combines high and low nybbles to create low byte of address
    POP R0       ; Low nybble of high address
    SUB R1,R0
-   LDI R3,HASH_TABLE[R0]
+   LDI R3,HASH_TABLE[R1]
    POP R0       ; High nybble of high address
    SUB R1,R0
-   LDI R4,HASH_TABLE[R0]
+   LDI R4,HASH_TABLE[R1]
    ADD R4,R3    ; Combines high and low nybbles to create high byte of address
    MOV R3,R1    ; Puts high byte address in correct place
-   PSH RF
-   PSH RE     ; PUTS BACK RETURN ADDRESS :)
+   PSH RB
+   PSH RA       ; PUTS BACK RETURN ADDRESS :)
    RTN
+
+
+CLEAR:
+   JSR CREATE_ADDRESS  ; Creates address from ASCII input
+   CLR R0              ; Clears R0
+   STI R0              ; Indexed store to memory, uses address in registers
+   JMP BEGIN           ; That was quick!
+
+
+BYTE:
+   LDC R1,0x02           ; Number of bytes of input to get
+   LDC R2,0x01           ; Value need for incrementing/decrementing
+   LDM R0,SERIAL_PORT_0  ; Gets byte from input
+   TST R0                ; test to see if byte contains anything (if not, nothing was fetched)
+   JPZ BYTE              ; try again
+   LDC RF,0x1B           ; ASCII value for ESC (Escape)
+   XOR RF,R0             ; Checks if input byte was an ESC
+   JPZ BEGIN             ; If the byte was an ESC, return to '>' prompt
+   SUB R1,R2             ; Decrement byte count
+   PSH R0                ; Pushes byte onto stack
+   JPZ WRITE             ; No more bytes to fetch so lets convert them
+   JMP BYTE              ; Get another byte
+   
+
+WRITE:
+   POP R1       ; Low nybble of data
+   LDC R0,0x30  ; Remove constant from ASCII value, makes table smaller.
+   SUB R0,R1    ; R0 = R0 - R1
+   LDI R2,HASH_TABLE[R1] ; Value of low nybble in R2
+   POP R0       ; High nybble of data
+   SUB R1,R0    ; Removes constant
+   LDI R3,HASH_TABLE[R1] ; Value of high nybble in R3
+   ADD R3,R2    ; Combines high and low nybbles to create low byte of address
+   MOV R2,R0
+   JSR CREATE_ADDRESS
+   STI R0       ; Uses address in R1(high byte) and R2(low byte)
+   JMP BEGIN
 
 
 HASH_TABLE:
@@ -165,40 +175,71 @@ HASH_TABLE:
    0x0D
    0x0E
    0x0F
-
-
-CLEAR:
-   JSR CREATE_ADDRESS  ; Creates address from ASCII input
-   CLR R0              ; Clears R0
-   STI R0              ; Indexed store to memory, uses address in registers
-   JMP BEGIN           ; That was quick!
-
-
-BYTE:
-   LDC R1,0x02           ; Number of bytes of input to get
-   LDC R2,0x01           ; Value need for incrementing/decrementing
-   LDM R0,SERIAL_PORT_0  ; Gets byte from input
-   TST R0                ; test to see if byte contains anything (if not, nothing was fetched)
-   JPZ BYTE              ; try again
-   LDC RF,0x1B           ; ASCII value for ESC (Escape)
-   XOR RF,R0             ; Checks if input byte was an ESC
-   JPZ BEGIN             ; If the byte was an ESC, return to '>' prompt
-   SUB R1,R2             ; Decrement byte count
-   PSH R0                ; Pushes byte onto stack
-   JPZ WRITE             ; No more bytes to fetch so lets convert them
-   JMP BYTE              ; Get another byte
    
+HASH_TABLE_BYTE:
+   0x30
+   0x31
+   0x32
+   0x33
+   0x34
+   0x35
+   0x36
+   0x37
+   0x38
+   0x39
+   0x00
+   0x00
+   0x00
+   0x00
+   0x00
+   0x00
+   0x00
+   0x41
+   0x42
+   0x43
+   0x44
+   0x45
+   0x46
 
-WRITE:
-   POP R0       ; Low nybble of data
-   LDC R1,0x30  ; Remove constant from ASCII value, makes table smaller.
-   SUB R1,R0    ; R0 = R0 - R1
-   LDI R2,HASH_TABLE[R0] ; Value of low nybble in R2
-   POP R0       ; High nybble of data
-   SUB R1,R0    ; Removes constant
-   LDI R3,HASH_TABLE[R0] ; Value of high nybble in R3
-   ADD R3,R2    ; Combines high and low nybbles to create low byte of address
-   MOV R2,R0
-   JSR CREATE_ADDRESS
-   STI R0       ; Uses address in R1(high byte) and R2(low byte)
-   JMP BEGIN   
+
+; Previous READ: ... what the hell was I thinking?!
+;   LDC R1,0x09
+;   SUB R0,R1           ; Does R1 = R1 - R0, if R0 is less than 9 then sign and zero not set.
+;   JPZ LOW_NYBBLE_30   ; Lower nybble is 9 so 0x30 must be added to create an ASCII byte
+;   JPS LOW_NYBBLE_37   ; R0 was greater than 9 so 0x37 must be added to create an ASCII byte
+;   JMP LOW_NYBBLE_30   ; R0 was less than 9 so nybble was less than 9, so 0x30 must be added to create an ASCII byte
+;   LOW_NYBBLE_30:
+;   LDC R1,0x30
+;   ADD R1,R0          ; Adds 0x30 to nybble to create ASCII data
+;   MOV R0,RE          ; Saves byte to be outputted
+;   JMP HIGH_NYBBLE
+;   LOW_NYBBLE_37:
+;   LDC R1,0x37
+;   ADD R1,R0           ; Adds 0x37 to nybble to create ASCII data
+;   MOV R0,RF           ; Saves byte to be outputted
+;   JMP HIGH_NYBBLE
+;   LDC R1,0xF0         ; Nybble of byte to be removed
+;   POP R0              ; Returns unaltered value
+;   AND R1,R0           ; Lower nybble removed
+;   SHR R0
+;   SHR R0
+;   SHR R0
+;   SHR R0              ; Shifted right four times to move it to lower nybble
+;   LDC R1,0x09
+;   SUB R0,R1           ; Does R1 = R1 - R0, if R0 is less than 9 then sign and zero not set.
+;   JPZ HIGH_NYBBLE_30  ; Lower nybble is 9 so 0x30 must be added to create an ASCII byte
+;   JPS HIGH_NYBBLE_37  ; R0 was greater than 9 so 0x37 must be added to create an ASCII byte
+;   JMP HIGH_NYBBLE_30  ; R0 was less than 9 so nybble was less than 9, so 0x30 must be added to create an ASCII byte
+;   HIGH_NYBBLE_30:
+;   LDC R1,0x30
+;   ADD R1,R0
+;   MOV R0,RE           ; Saves byte to be outputted
+;   JMP OUTPUT          ; Wooo!
+;   HIGH_NYBBLE_37:
+;   LDC R1,0x37
+;   ADD R1,R0
+;   MOV R0,RE           ; Saves byte etc
+;   OUTPUT:
+;   STM RE,SERIAL_PORT_0  ; Ouputs high nybble
+;   STM RF,SERIAL_PORT_0  ; Outputs low nybble
+;   JMP BEGIN
