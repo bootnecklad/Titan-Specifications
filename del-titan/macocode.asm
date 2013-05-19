40e3fdad90501a7e1993b92bae55337c22f4d380
bootnecklad <bootnecklad@gmail.com>
2013-05-19 16:03:24 +0100

    rewritten while talking to Quad

--- del-titan/macocode.asm
+++ del-titan/macocode.asm
@@ -1,74 +1,145 @@
-LOCAL:
-   LDC R0,0xnn   ; loads n
-   MOV RD,R2
-   MOV RC,R1     ; saves stack pointer
-   ADD R0,RD
-   JPC LocalCarry
-   JMP LocalPerform
-  LocalCarry:
-   INC RC
-  LocalPerform:
-   POP R0  ; where does the value go after this?
-   
-INT_TO_DEL:
-   LDC R0,0xnn   ; loads value to convert to tagged pointer format
-   ADD R0,R0     ; Shifts value left
+IntToDel:
+   CLR R2
+   SHL R1
+   JPC ITDCarry
+   JMP ITDNoCarry
+ ITDCarry:
+   LDC R2,0x01
+ ITDNoCarry:
+   SHL R0
+   LOR R2,R0
 
-PRIM_PRINT:
-;;; i have no idea how to convert a binary number into ASCII decimal. Help.
+DelToInt:
+   CLR R2
+   SHR R0
+   JPC DTICarry
+   JMP DTINoCarry
+ DTICarry:
+   LDC R2,0x80
+ DTINoCarry:
+   SHL R1
+   LOR R2,R1
 
-GC_IF_NEEDED:
-;;; i dont know how to write a garbage collector
+PrtToDel:
+   INC R1
 
+DelToPtr
+   DEC R1
 
-;;; RA holds the current heap pointer
-PUSH_CLOSURE:
-   LDC R1,0xnn    ; constant to beginning of heap pointer, assuming heap is only 256 bytes big
-   STM R0,[R1,RA] ; stores address to heap
-   PSH RA         ; pushes the pointer to the heap onto the stack
-
-PEEK:
-   POP R0
+Push:
+   PSH R1
    PSH R0
-   PSH R0 ; performs a PEEK.
 
-GLOBAL:
-   LDC R1,0xnn  ; loads location n of the global list
-   LDC R2,0xnn  ; constant to beginning of global list, assuming global list is only 256 bytes big
-   STI R0,[R2,R1] ; stores to global list
-
-RESET_STACK:
-   CLR RC
-   CLR RD ; resets stack pointer
-
-;;; RA holds the current heap pointer
-SAVE_TOP:
+Pop:
    POP R0
-   PSH R0 ; ensures that value is not lost
-   LDC R1,0xnn  ; constant to beginning of heap pointer, assuming heap is only 256 bytes big
-   STM R0,[R1,RA] ; stores value to heap
+   POP R1
 
-PRIM_EQ:
+Peek:
    POP R0
    POP R1
    PSH R1
-   PSH R0 ; takes values off the top of the stack and restores them
-   XOR R0,R1
-   JPZ Equal
-   LDC R0,0x00
    PSH R0
-   JMP Finish
-  Equal:
-   LDC R0,0x02
-   PSH R0
-  Finish:
 
-TOP:
+Halt:
+   JMP Halt
+
+   
+; [R0,R1] should contain the number of bytes needed
+GCIfNeeded:
+
+.BYTE HeapENDHighByte 0xDF
+.BYTE HeapENDLowByte 0xFF  
+
+   LDC R2,HeapENDHighByte
+   LDC R3,HeapENDLowByte
+   ADD R8,R1
+   JPC GCFixCarry
+   JMP GCNoCarry
+ GCFixCarry:
+   INC R1
+ GCNoCarry:
+   ADD R9,R0
+   INC R1
+   JPC GCFixCarryInc
+   JMP GCNoCarryInc
+ GCFixCarryInc:
+   INC R0
+ GCNoCarryInc:    
+   SUB R1,R3  ; subtracts low bytes
+   JPC SUBFIX ; corrects for first byte overflow
+   SUB R0,R2  ; subtracts high bytes
+   JMP SUBEND ; finishes addition
+ SUBFIX:
+   DEC R0     ; corrects for overflow
+   SUB R0,R2  ; subtracts high bytes
+ SUBEND:
+   JPS GCNotNeeded     ; [R2,R3] contains 
+   JMP Halt ; RUH ROH
+ GCNotNeeded:
+
+; allocate and store [R0,R1] to the heap
+AllocateAndStore:
+   LDC R2,0x02
+   ADD R2,R9
+   JPC AllocateCarryFix
+   JMP AllocateNoCarryFix
+ AllocateCarryFix:
+   INC R8
+ AllocateNoCarryFix:
+   STI R0,[R8,R9]
+   INC R9
+   STI R1,[R8,R9]
+   DEC R9
+   RTN
+
+; save top of stack to the heap
+SaveTop:
+   POP R0
    POP R1
-   PSH R0  ; sets top of stack to contents of R0
+   JPS AllocateAndStore
 
-REF:
-   POP R1 ; gets pointer to some where in heap
-   INC R1 ; increments the pointer
-   LDC R2,0xnn ; constant to beginning of heap pointer, assuming heap is only 256 bytes big
-   LDI R0,[R2,R1] ; gets value at offset
+; assume address is in [R0,R1]
+; saves address to heap
+; increments the address
+; stores address on stack
+PushClosure:
+   JPS AllocateAndStore
+   INC R1
+   PSH R1
+   PSH R0
+
+; index is in index register [R6,R7]
+; dont close more than 256 variables
+Ref:
+   POP R0
+   POP R1
+   PSH R1
+   PSH R0   ; performs a peek
+   DEC R1   ; performs DEL TO PTR
+   ADD R7,R1
+   JPC RefCarryFix
+   JMP RefNoCarryFix
+ RefCarryFix:
+   INC R0
+ RefNoCarryFix:   ; adds offset from index register
+   LDI R3,[R0,R1]
+   INC R1
+   LDI R2,[R0,R1]   ; gets value of a closed variable from memory
+   PSH R3
+   PSH R2 ; pushes onto stack
+ 
+ResetStack:
+   CLR RC
+   CLR RD ; clears stack pointer
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
