9c81b00e2ecd94f584359f7c15d2d887960ecf19
bootnecklad@gmail.com <marc@bnl-mbp.local>
2015-02-26 07:31:20 +0000

    transformer based assembler outputs correct machine code now

--- assembler/assembler.scm
+++ assembler/assembler.scm
@@ -13,6 +13,9 @@
 (use (srfi 1))
 (define nil '())
 
+;;; errors out when something is not implemented
+(define (not-implemented) (error "not implemented yet"))
+
 ;; Different components of a Titan program
 (define (directive? instr)
   (and (pair? instr)
@@ -24,8 +27,6 @@
   (and (pair? instr)
        (eq? '.LABEL (car instr))))
 
-; (define (label? instr)
-; (symbol? instr))
 (define (instruction? instr)
   (and (pair? instr)
        (not (directive? instr))))
@@ -53,16 +54,17 @@
   (extract directive? prog))
 
 ;;; defines the lengths of the different opcodes
-(define opcode-lengths '( (NOP 1) (ADD 2) (ADC 2) (SUB 2) (AND 2) (LOR 2) (XOR 2) (NOT 2)
-                          (SHR 2) (INC 2) (DEC 2) (INT 2) (RTE 1) (CLR 1) (PSH 1) (POP 1)
+(define opcode-lengths '( (NOP 1) (HLT 1) (ADD 2) (ADC 2) (SUB 2) (AND 2) (LOR 2) (XOR 2) (NOT 2)
+                          (SHR 2) (INC 2) (DEC 2) (INT 2) (RTE 1) (CLR 1) (PSH 1) (POP 1) (PEK 1)
                           (MOV 2) (JMP 3) (JPZ 3) (JPS 3) (JPC 3) (JPI 3) (JSR 3) (RTN 1)
                           (JMI #f) (LDI #f) (STI #f) (LDC 2) (LDM 3) (STM 3)))
 
 ;;; defines the machine code values for different opcodes
-(define opcodes '((NOP #x00) (ADD #x10) (ADC #x11) (SUB #x12) (AND #x13) (LOR #x14) (XOR #x15) (NOT #x16)
-                  (SHR #x17) (INC #x18) (DEC #x19) (INT #x20) (RTE #x21) (CLR #x60) (PSH #x70) (POP #x80)
-                  (MOV #x90) (JMP #xA0) (JPZ #xA1) (JPS #xA2) (JPC #xA3) (JPI #xA4) (JSR #xA5) (RTN #xA6)
-                  (LDC #xD0) (LDM #xE0) (STM #xF0) (JMI #f) (LDI #f) (STI #f) (TST #f) (SHL #f)))
+(define opcodes '((NOP #x00) (HLT #x01) (ADD #x10) (ADC #x11) (SUB #x12) (AND #x13) (LOR #x14) (XOR #x15)
+		  (NOT #x16) (SHR #x17) (INC #x18) (DEC #x19) (INT #x20) (RTE #x21) (CLR #x60) (PSH #x70) 
+		  (POP #x80) (PEK #x90) (MOV #x90) (JMP #xA0) (JPZ #xA1) (JPS #xA2) (JPC #xA3) (JPI #xA4)
+		  (JSR #xA5) (RTN #xA6) (LDC #xD0) (LDM #xE0) (STM #xF0) 
+		  (JMI #f) (LDI #f) (STI #f) (TST #f) (SHL #f)))
 
 
 ;;; defines the machine code values for different registers
@@ -72,6 +74,13 @@
 ;;; defines all the directives
 (define directives '(.RAW .LABEL .BYTE .WORD .LABEL .DATA .ASCII .ASCIZ))
 
+;;; gets length of instr, whether that be an instructions, label or directive
+(define (compute-length instr)
+  (cond
+   ((label? instr) 0)
+   ((instruction? instr) (instr-length instr))
+   ((directive? instr) (compute-directive-length instr))
+   (else (error "INVALD TITAN ASM"))))
 
 ;;; gets the length of executable instruction (in bytes)
 (define (instr-length instr)
@@ -80,15 +89,6 @@
           (cadr (member (car instr) (flatten opcode-lengths)))
           (compute-instruction-length instr))
       (begin (display (car instr)) (error "INVALID TITAN INSTRUCTION"))))
-
-;;; gets length of instr, whether that be an instructions, label or directive
-
-(define (compute-length instr)
-  (cond
-   ((label? instr) 0)
-   ((instruction? instr) (instr-length instr))
-   ((directive? instr) (compute-directive-length instr))
-   (else (error "INVALD TITAN ASM"))))
 
 ;;; computes the lengths of special instructions
 (define (compute-instruction-length instr)
@@ -113,11 +113,6 @@
     ((.ASCIZ) (+ 1 (string-length (caddr instr))))
     (else (error "INVALID DIRECTIVE"))))
 
-
-;;; errors out when something is not implemented
-(define (not-implemented) (error "not implemented yet"))
-
-;;; quad
 ;;; loops through assembly program assigning lengths to instructions
 (define (apply-lengths prog)
   (map compute-length prog))
@@ -133,29 +128,15 @@
       (cons (+ sum (car lst))
             (running-total (cdr lst) (+ sum (car lst))))))
 
-
-;;; extracts high byte of index
-(define (highbyte v)
-  (vector-ref v 0))
-
-;;; extracts low byte of index
-(define (lowbyte v)
-  (vector-ref v 1))
-
 ;;; tells us if an operand is an index, ie (Rh Rl)
 (define (word? v)
   (vector? v))
 
-;;; begins converting instructions into machine code values
-(define (begin-convert prog)
-  (map convert prog))
-
 ;;; begins converting process
 (define (convert instr)
   (cond
-   ((label? instr) instr)
    ((instruction? instr) (convert-instr instr))
-   ((directive? instr) (convert-directive instr))
+   ((directive? instr) (cdr instr))
    (else (error "INVALD TITAN ASSEMBLY"))))
 
 ;;; converts assembly instructions into machine code values
@@ -166,7 +147,6 @@
                 (cdr instr))
           (convert-special-instr instr))
       (begin (display instr) (error "INVALID TITAN INSTRUCTION"))))
-
 
 ;;; converts special instructions
 (define (convert-special-instr instr)
@@ -180,55 +160,18 @@
         ((LDI) (cons #xB0 (cdr instr)))
         ((STI) (cons #xC0 (cdr instr))))))
 
-
-;;; converts directives into machine code form
-(define (convert-directive instr)
-  (case (car instr)
-    ((.RAW) (cdr instr))
-    ((.BYTE .WORD) instr)
-    ((.DATA) (cddr instr))
-    ((.LABEL) instr)
-    ((.ASCII) (map char->integer (string->list (caddr instr))))
-    ((.ASCIZ) (append (map char->integer (string->list (caddr instr))) '(0)))
-    (else (error "INVALID TITAN DIRECTIVE"))))
-
-(define (label-in-instr? label instr)
-  (and (directive? instr)
-       (eq? label (cadr instr))))
-
-(define (get-position label prog pos)
-  (cond
-   ((null? prog) #f)
-   ((label-in-instr? label (list-ref prog pos)) pos)
-   (else (get-position label prog (+ 1 pos)))))
-
-;;; gets the nth item in a list
-(define (get-nth n lst)
-  (if (>= n (length lst))
-      nil
-      (if (eq? n 0)
-          (car lst)
-          (get-nth (- n 1) (cdr lst) ))))
-
-(define (extract-directives lst)
-  (map cadr lst))
-
-(define (extract-addr lst prog offset)
-  (if (null? lst)
-      nil
-      (cons (get-nth (get-position (car lst) prog 0) (add-offset offset (address-list prog)))
-            (extract-addr (cdr lst) prog offset))))
-
-;;; #;4> (map list '(1 2 3 4) '(5 6 7 8))
-;;; ((1 5) (2 6) (3 7) (4 8))
-(define (create-subs prog offset)
-  (map list (extract-directives (get-directives prog))
-       (extract-addr (extract-directives (get-directives prog)) prog offset)))
-
 (define (substitute-all lst tbl)
   (if (null? tbl)
       lst
       (substitute-all (substitute lst (caar tbl) (cadar tbl)) (cdr tbl))))
+
+(define (substitute prog label value)
+  (map (lambda (element)
+	 (cond
+	  ((eq? label element) value)
+	  ((list? element) (substitute element label value))
+	  (else element)))
+       prog))
 
 ;;; adds offset to address-list
 (define (add-offset offset lst)
@@ -260,16 +203,22 @@
             (remove-directives (cdr prog)))))
 
 ;;; prints DA PROG
-(define (print-bytes bytes n)
+(define (print-bytes bytes addr n)
   (cond
    ((null? bytes) (newline))
-   ((zero? (remainder n 16)) (newline) (print-byte (car bytes)) (print-bytes (cdr bytes) (+ n 1)))
-   (else (print-byte (car bytes)) (print-bytes (cdr bytes) (+ n 1)))))
+   ((zero? (remainder n 8)) (newline) (print-word addr) (display ": ") (print-byte (car bytes)) (print-bytes (cdr bytes) (+ addr 8) (+ n 1)))
+   (else (print-byte (car bytes)) (print-bytes (cdr bytes) addr (+ n 1)))))
 
 (define (print-byte n)
   (display (string-upcase (if (> n #xF)
                               (sprintf "~X " n)
                               (sprintf "0~X " n)))))
+(define (print-word n)
+  (display (string-upcase (cond
+			   ((<= n #xF) (sprintf "000~X " n))
+			   ((<= n #xFF) (sprintf "00~X " n))
+			   ((<= n #xFFF) (sprintf "0~X " n))
+			   ((> n #xFFF) (sprintf "~X" n))))))
 
 
 ;;; prints length of program in bytes
@@ -280,7 +229,7 @@
 ;;; this is the function that you actually call
 (define (assemble prog offset)
   (print-length (assembler prog offset))
-  (print-bytes (assembler prog offset) 0))
+  (print-bytes (assembler prog offset) offset 0))
 
 (define (desugar-directive directive)
   (case (car directive)
@@ -401,19 +350,12 @@
 	 (prog-four/env (alias-environment prog-three))
 	 (prog-four (first prog-four/env))
 	 (env (second prog-four/env))
-	 (prog-five (map (lambda (instr) (substitute-instruction instr env)) prog-four)))
-    prog-five))
-    
-
-;;; THE BIG DIRTY
-;(define (assembler prog offset)
-;  (flatten
-;   (remove-directives
-;    (merge prog
-;           (substitute-all (substitute-all (begin-convert prog)
-;                                           (create-subs prog offset))
-;                           registers)))))
-
+	 (prog-five (map (lambda (instr) (substitute-instruction instr env)) prog-four))
+	 (prog-six (map convert prog-five))
+	 (prog-seven (substitute-all prog-six registers))
+	 (prog-eight (merge prog-five prog-seven))
+	 (prog-nine (flatten prog-eight)))
+    prog-nine))
 
 ;;; does all the dirty work
 (define (do-merge orig-instr instr)
